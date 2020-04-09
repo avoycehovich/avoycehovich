9b2d1248aeb661d13fa1efaa4fb4f006ee46fb38
Arkadiy <axsl.voytcehovich@gmail.com>
2020-04-09 09:56:14 +0300

    home6

---
+++ home6/src/js/main.js
@@ -0,0 +1,22 @@
+let startBnt = document.getElementById('start');
+let budgetValue = document.getElementsByClassName('budget-value')[0];
+let dayBudgetValue = document.getElementsByClassName('daybudget-value')[0];
+let levelValue = document.getElementsByClassName('level-value')[0];
+let expensesValue = document.getElementsByClassName('expenses-value')[0];
+let optExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0];
+let incomeValue = document.getElementsByClassName('income-value')[0];
+let monthSavingValue = document.getElementsByClassName('monthsavings-value')[0];
+let yearsSavingValue = document.getElementsByClassName('yearsavings-value')[0];
+
+let expensesInput = document.getElementsByClassName("expenses-item");
+
+let approveBnt = document.getElementsByTagName('button')[1];
+let calcBtn = document.getElementsByTagName('button')[2];
+let optExpensesItems = document.querySelectorAll('.optionalexpenses-item');
+let chooseIncome = document.querySelector('.choose-income');
+let savings = document.querySelector('#savings');
+let chooseSum = document.querySelector('.choose-sum');
+let choosePerc = document.querySelector('.choose-percent');
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
