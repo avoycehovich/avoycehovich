1bbc38fc4766cc05c5fe4cc577b9580eee02590a
Arkadiy Voycehovich <avoycehovich@axiomsl.com>
2020-04-21 14:02:00 +0300

    first react app -> props and state

--- react_course/first_react_app/my-app/src/index.js
+++ react_course/first_react_app/my-app/src/index.js
@@ -1,43 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
+import React, {Component} from 'react'
+import ReactDom from 'react-dom'
+
+import './index.css'
 
 
-const Header = () => {
-    return <h2>Hello World</h2>;
-};
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 26
+        };
+        // this.nextYear = this.nextYear.bind(this);
+        this.nextYear = () => {
+            this.setState(state => ({
+                years: ++state.years
+            }))
+        }
+    }
 
-const Field = () => {
-    const holder = 'Enter here';
-    const stylesField = {
-        width: "300px"
-    };
-    return <input
-        style={stylesField}
-        type="text"
-        placeholder={holder}
-        autoComplete=""
-        className="first"
-    />;
-};
+    // nextYear() {
+    //     this.setState(state => ({
+    //         years: ++state.years
+    //     }))
+    // }
+    render() {
+        const {name, surname, link} = this.props;
+        const {years} = this.state;
+        return (
+            <>
+                <button onClick={this.nextYear}>++</button>
+                <h1>My name is {name}, surname - {surname}, years = {years}</h1>
+                <a href={link}>My profile</a>
+            </>
+        );
+    }
+}
 
-const Btn = () => {
-    const text = 'Log in';
-    const logged = true;
-    return <button>{logged ? "Enter" : text}</button>;
-};
-
-const App = () => {
+const All = () => {
     return (
-        <div>
-            <Header/>
-            <Field/>
-            <Btn/>
-        </div>
+        <>
+            <WhoAmI name="John" surname={"Smith"} link={"facebook.com"}/>
+            <WhoAmI name="Ivan" surname={"Smith"} link={"facebook.com"}/>
+            <WhoAmI name="Vera" surname={"Smith"} link={"facebook.com"}/>
+        </>
     )
 };
 
-ReactDOM.render(<App/>, document.getElementById('root')
-);
-
+ReactDom.render(<All/>, document.getElementById("root"));
