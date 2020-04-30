cbc6add8d0d54dbcf37471e92977f78b36d6aebe
Arkadiy Voycehovich <avoycehovich@axiomsl.com>
2020-04-30 16:00:24 +0300

    added total price

--- react_course/resto/my-app/src/reducers/index.js
+++ react_course/resto/my-app/src/reducers/index.js
@@ -3,13 +3,14 @@ const initState = {
     loading: true,
     error: false,
     items: [],
-    oneItemCount: 0
+    oneItemCount: 0,
+    totalPrice: 0
 };
 
 const reducer = (state = initState, action) => {
-    console.log(state);
     switch (action.type) {
         case 'MENU_LOADED':
+            console.log(`Loaded -> Total: ${state.totalPrice}`);
             return {
                 ...state,
                 menu: action.payload,
@@ -39,23 +40,29 @@ const reducer = (state = initState, action) => {
                 url: item.url,
                 id: item.id
             };
+            console.log(`Add -> new item price: ${newItem.price}`);
+            const newTotalPrice = state.totalPrice + newItem.price;
+            console.log(`Add -> TotalPrice: ${newTotalPrice}`);
             return {
                 ...state,
                 items: [
                     ...state.items,
                     newItem
-                ]
+                ],
+                totalPrice: newTotalPrice
             };
         case 'ITEM_REMOVE_FROM_CARD':
             const index = action.payload;
             const itemIndex = state.items.findIndex(item => item.id === index);
+            const newPrice = state.totalPrice - state.items[itemIndex].price;
             return {
                 ...state,
                 items: [
                     ...state.items.slice(0, itemIndex),
                     ...state.items.slice(itemIndex + 1)
-                ]
-            }
+                ],
+                totalPrice: newPrice
+            };
         default:
             return state;
     }
